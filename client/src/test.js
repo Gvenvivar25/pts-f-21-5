5b340a106c1f053cf7dfed71c993efe854b8c015
Arni01 <fkpmikpres@gmail.com>
2021-12-11 18:05:34 +0300

    feat: added click for tag

--- client/src/test.js
+++ client/src/test.js
@@ -1,11 +1,24 @@
 // import { createElement } from '../react-shim'
 // import { el } from './react/react'
 
-const elem = ({ name }) => (
-  <div>
-    <h3>Hello {name ? name : 'world'}</h3>
-    <p>I hope you're having a good day</p>
-  </div>
-)
+let a = 0
+console.log(a)
+
+const elem = ({ name }) => {
+  return (
+    <div>
+      <h3>Hello {name ? name : 'world'}</h3>
+      <p>I hope you're having a good day {a}</p>
+      <button
+        onClick={() => {
+          console.log('button')
+          a++
+        }}
+      >
+        click
+      </button>
+    </div>
+  )
+}
 
 export default elem

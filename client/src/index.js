4ec063a55dc2c2f8e708e4a61aa45f44a483ba67
Arni01 <fkpmikpres@gmail.com>
2021-12-17 20:24:14 +0300

    fix(css): fixed bug with dysplaying styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import App from './app'
 import { mount } from './react/newVersion/mounting'
 import { createElement } from './react/newVersion/creating'
-import './styles/style.sass'
+import './styles/style.css'
 
 mount(createElement(App), window.root)

52ab37fb35f20a77ef9237063a11ed8b5d9b6da6
Arni01 <fkpmikpres@gmail.com>
2021-12-20 05:54:57 +0300

    fix(deploy): fixed style

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import App from './app'
 import { mount } from './react/newVersion/mounting'
 import { createElement } from './react/newVersion/creating'
-import './styles/style.sass'
-import './components/Product/item.scss'
+import './styles/style.css'
+// import './components/Product/item.scss'
 
 mount(createElement(App), window.root)

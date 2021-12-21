42b78afbb2cd35fc36bbd8b3438260d92a733a71
Stepan <Stteppa9092@gmail.com>
2021-12-21 08:21:12 +0300

    feat: add links in shopping cart, wishlist

--- client/src/components/ShoppingCart/CartItem.js
+++ client/src/components/ShoppingCart/CartItem.js
@@ -9,11 +9,11 @@ class CartItem extends Component {
         const { product } = this.props.data
         return (
               <li class="cart_item">
-                <a href="" class="link_img">
+                <a href={`/product?id=${product.id}`} class="link_img">
                   <img class="cart_img" src={product.image} alt=""></img>
                 </a>
                 <div class="cart_content">
-                  <h3 class="cart_item_name"><a href="" class="cart_link_name">{product.name}</a></h3>
+                  <h3 class="cart_item_name"><a href={`/product?id=${product.id}`} class="cart_link_name">{product.name}</a></h3>
                   <span class="cart_price">{product.sign} {product.price}</span>
                   <button  onClick={()=>this.props.deleteItem(product.id)} class="cart_remove">remove</button>
                 </div>
--- client/src/components/Wishlist/WishItem.js
+++ client/src/components/Wishlist/WishItem.js
@@ -10,14 +10,14 @@ class WishItem extends Component {
         const { product } = this.props.data
         return (
         <li class="wish_item">
-            <a href="" class="link_img">
+            <a href={`/product?id=${product.id}`} class="link_img">
                 <img class="wish_img"
                     src={product.image}
                     alt={product.name}
                 />
             </a>
             <div class="wish_content">
-                <h3 class="wish_item_name"><a href="" class="wish_link_name">{product.name}</a></h3>
+                <h3 class="wish_item_name"><a href={`/product?id=${product.id}`} class="wish_link_name">{product.name}</a></h3>
                 <div class="wish_block">
                     <div class="wish_price">{product.sign} {product.price}</div>
                     {JSON.parse(localStorage.getItem('cart')).includes(product.id)?<button class="wish_in_cart">in cart</button>:<button onClick={()=>this.props.addInCart(product.id)} class="wish_add">add to cart</button>}
